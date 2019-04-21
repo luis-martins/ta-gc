994940ae00f09058b02270deb629d41ec1be21ae
luismartins.e <luismartins.e@gmail.com>
2019-04-21 06:03:05 +0100

    WebODM results

---
+++ results/potree_pointcloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 15088402,
+    "boundingBox": {
+        "lx": 554033.63,
+        "ly": 4505756.74,
+        "lz": -17.77,
+        "ux": 554449.51,
+        "uy": 4506172.62,
+        "uz": 398.1100000000047
+    },
+    "tightBoundingBox": {
+        "lx": 554033.63,
+        "ly": 4505756.74,
+        "lz": -17.77,
+        "ux": 554449.51,
+        "uy": 4506092.79,
+        "uz": 61.300000000000007
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 2.881301164627075,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
